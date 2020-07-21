9b74995c5c8095be633d7b82c8ed0c2bb530c107
Nipun Sampath <nipunsampath@gmail.com>
2020-07-21 17:07:45 +0530

    code cleanup

--- app/src/main/java/com/dnn/vision/Reciver.java
+++ app/src/main/java/com/dnn/vision/Reciver.java
@@ -3,35 +3,20 @@ package com.dnn.vision;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 
-class EventReciever extends BroadcastReceiver {
+class EventReceiver extends BroadcastReceiver {
 
 
-    private SharedPreferences prefAppStartKey;
-    private  SharedPreferences.Editor editorAppStartKey;
+    EventReceiver()
+    {
+    }
 
     public void onReceive(final Context context, final Intent intent) {
-        prefAppStartKey = context.getApplicationContext().getSharedPreferences("AppStartKey", 0); // 0 - for private mode
-        editorAppStartKey = prefAppStartKey.edit();
-
-//        Log.e("LOB", "Value "+prefAppStartKey.getInt("AppStartKey", 1)+"");
 
         if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-            if(prefAppStartKey.getInt("AppStartKey", 0) == 0) {
                 Intent i = new Intent(context, SplashScreenActivity.class);
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(i);
-            }
-
-        }
-        else if (intent.getAction().equals(Intent.ACTION_MEDIA_MOUNTED)) {
-
-            if(prefAppStartKey.getInt("AppStartKey", 0) == 1) {
-                Intent i = new Intent(context, MainActivity.class);
-                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(i);
-            }
 
         }
     }
